17916c66c4dcc0f03b2eea0ad55fd4f341dbaa36
Shreeansh_Gupta@epam.com <shreeansh.gupta@daimler.com>
2021-04-20 13:29:13 +0530

    add test for comment-box component

--- src/__tests__/ComponentBox.test.js
+++ src/__tests__/ComponentBox.test.js
@@ -17,3 +17,29 @@ it("shows the textarea and button", () => {
   expect(component.find("textarea").length).toEqual(1);
   expect(component.find("button").length).toEqual(1);
 });
+
+describe("the text area", () => {
+  // this before each will be scoped only for this describe block
+  beforeEach(() => {
+    component.find("textarea").simulate("change", {
+      target: { value: "New Comment" },
+    });
+
+    component.update();
+  });
+
+  it("allows user to enter text in textarea", () => {
+    // change event is the html change that should be added.
+    // this test only works when there is controlled input/textarea
+    expect(component.find("textarea").prop("value")).toEqual("New Comment");
+  });
+
+  it("submit the text and empty the text area", () => {
+    // test to simulate form submit and make sure that textarea is empty
+    expect(component.find("textarea").prop("value")).toEqual("New Comment");
+
+    component.find("form").simulate("submit");
+    component.update();
+    expect(component.find("textarea").prop("value")).toEqual("");
+  });
+});
